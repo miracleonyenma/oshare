97c20f739d294dbaae4bdf232a75bd9cb4e3216d
miracleonyenma <miracleficient@gmail.com>
2021-01-04 00:50:03 +0100

    update && feat: add basic structure, import fileInput Component

--- defaultFE/src/App.vue
+++ defaultFE/src/App.vue
@@ -1,26 +1,46 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="main">
+    <file-input class="input-comp"></file-input>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FileInput from "./components/fileInput.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    FileInput
   }
-}
+};
 </script>
 
 <style>
+button{
+  cursor: pointer;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+
+.btn{
+  outline: none;
+  border: none;
+  padding: 1em 1.5em;
+  border-radius: .5em;
+  transition: all 0.32s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+}
+
+.btn:hover{
+  transform: scale(1.1);
+}
+
+.btn:active{
+  transform: scale(0.9);
 }
 </style>
