2b737151c9c7d61e6eb4bc1fb4037c25c9a8aab7
miracleonyenma <miracleficient@gmail.com>
2021-01-04 00:50:30 +0100

    update && feat: create functional fileInput component

---
+++ defaultFE/src/components/fileInput.vue
@@ -0,0 +1,81 @@
+<template>
+    <div class="container">
+        <div class="input-wrapper">
+            <label for="fileInput">Choose File</label>
+            <input type="file" name="fileInput" id="fileInput" ref="fileInput" v-on:change="handleFileUpload()">
+            <!-- ref is used to register a reference to an element or a child component. The reference will be registered under the parent component’s $refs object -->
+        </div>
+        <button class="btn" v-on:click="submitFile()">Upload</button>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios';
+
+    export default {
+        data() {
+            return {
+                // store file data
+                file: ''
+            }
+        },
+
+        methods: {
+            handleFileUpload() {
+                // get first file from input ref once changed
+                this.file = this.$refs.fileInput.files[0];
+                console.log(this.file);
+
+            },
+            submitFile() {
+                let formData = new FormData();
+
+                // https://developer.mozilla.org/en-US/docs/Web/API/FormData/append
+                formData.append('file', this.file);
+
+                const sendFile = async () =>{
+                    try{
+                        const res = axios.post('/single-file',
+                            formData,
+                            {
+                                headers: {
+                                    'Content-Type': 'multipart/form-data'
+                                }
+                            }
+                        );
+
+                        console.log(res.data);
+
+                    } catch(err){
+                        console.error(err)
+                    }
+                };
+
+                sendFile();
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .container {
+        max-width: 100%;
+        padding: 3em;
+    }
+
+    .input-wrapper{
+        background: #efefef;
+        border-radius: 1em;
+        padding: 2em 1em;
+        margin-bottom: 2em;
+    }
+
+    input[type="file"]{
+        outline: none;
+    }
+
+    label[for="fileInput"]{
+        display: block;
+        margin-bottom: 1em;
+    }
+</style>
