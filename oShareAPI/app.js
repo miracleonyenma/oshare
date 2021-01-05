e2260a0e4ea7055b9ef7053746c3127252fd5fd0
miracleonyenma <miracleficient@gmail.com>
2021-01-05 22:58:57 +0100

    update & feat: update single-file route, minimal err handling

--- oShareAPI/app.js
+++ oShareAPI/app.js
@@ -1,8 +1,33 @@
 const express = require("express");
-const axios = require("axios");
-const expressFile = require("express-fileupload");
+const fileupload = require("express-fileupload");
+const fs = require("fs");
+const cors = require("cors");
+
+// var multer  = require('multer')
+// var upload = multer({ dest: 'uploads/' })
+
+const port = `3000`;
 
 const app = express();
+
+app.use(express.json());
+
+//Parse URL-encoded bodies
+// app.use(express.urlencoded({extended: true}));
+
+// This is needed because otherwise the server can’t parse file uploads.
+app.use(fileupload());
+
+// const storage = multer.diskStorage({
+//     destination: (req, file, cb)=>{
+//         cb(null, 'uploads/')
+//     },
+//     filename: (req, file, cb)=>{
+//         cb(null, file.mimetype + Date.now)
+//     }
+// });
+
+// const upload = multer({storage : storage});
 
 
 app.get("/", (req, res) => {
@@ -10,17 +35,62 @@ app.get("/", (req, res) => {
     res.send("Hola")
 });
 
-app.post("/single-file", (req, res)=>{
+
+app.post("/single-file", cors(), (req, res) => {
+
+    console.log(!!req.files);
+    if(!req.files){
+        res.status(500).json({
+            status: "error",
+            message: "Please upload a file, no file detected"
+        })
+
+        return
+    }
     const file = req.files.file;
-    const path = __dirname + '/files' + file.name;
+    const path = `${__dirname}/uploads/${file.name}`;
 
-    console.log(file.name);
-})
+    //move file
+    file.mv(path, err=>{
+        if(err){
+            console.error(err);
+            res.writeHead(500, {
+                'Content-Type' : 'application/json'
+            })
+            res.end(JSON.stringify({
+                status: "error",
+                message: err
+            }))
 
-// This is needed because otherwise the server can’t parse file uploads.
-app.use(expressFile);
-
-
-app.listen(3000, ()=>{
-    console.log("app running on localhost:3000")
+            return
+        }
+        res.writeHead(200, {
+            'Content-Type' : 'application/json'
+        })
+        res.end(JSON.stringify({
+            status: "success",
+            message: {
+                file: file.name,
+                path: path
+            }
+        }))
+    })
 });
+
+// app.post('/uploadfile', cors(), upload.single('myFile'), (req, res, next) => {
+//     const file = req.file
+//     console.log(file)
+//     if (!file) {
+//         const error = new Error('Please upload a file')
+//         error.httpStatusCode = 400
+//         return next(error)
+//     }
+//     res.send(file);
+// })
+
+
+app.listen(port, () => {
+    console.log(`app running on http://localhost:${port}`);
+});
+
+//https://robkendal.co.uk/blog/how-to-build-a-restful-node-js-api-server-using-json-files
