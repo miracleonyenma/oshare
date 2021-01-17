f8fc31d9db05fcd4e7534ed56d2cea9e02a10529
miracleonyenma <miracleficient@gmail.com>
2021-01-17 23:30:06 +0100

    feat: update file gallery comp to render list from /files route

--- defaultFE/src/components/FileGallery.vue
+++ defaultFE/src/components/FileGallery.vue
@@ -1,14 +1,8 @@
 <template>
   <section>
       <ul class="file-gallery">
-          <li class="file-gallery__item">
-              <p>filename</p>
-          </li>
-          <li>
-              <p>filename</p>
-          </li>
-          <li>
-              <p>filename</p>
+          <li class="file-gallery__item glass glass--main-2" v-for="(file, i) in filesData" v-bind:key="file[i]">
+              {{ file }}
           </li>
       </ul>
   </section>
@@ -16,6 +10,15 @@
 
 <script>
 export default {
+    props: {
+        filesData : Array
+    },
+
+    data(){
+        return {
+
+        }
+    }
 
 }
 </script>
@@ -23,9 +26,17 @@ export default {
 <style lang="scss" scoped>
     .file-gallery{
         display: flex;
+        width: 100%;
+        flex-wrap: wrap;
+        gap: 1em;
+
 
         &__item{
-
+            padding: 1em;
+            // background: var(--main);
+            color: var(--bg);
+            border-radius: 5px;
+            // box-shadow: 6px 6px 12px var(--main-trans);
         }
     }
 </style>
