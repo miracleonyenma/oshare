600294ce665cf3e965b0cb9536c5d12b0bb260e4
miracleonyenma <miracleficient@gmail.com>
2021-01-22 20:20:16 +0100

    update: change border-radius of elements, lint file

--- defaultFE/src/components/FileGallery.vue
+++ defaultFE/src/components/FileGallery.vue
@@ -1,41 +1,39 @@
 <template>
-  <section>
-      <ul class="file-gallery">
-          <li class="file-gallery__item glass glass--main-2" v-for="(file, i) in filesData" v-bind:key="file[i]">
-              {{ file }}
-          </li>
-      </ul>
-  </section>
+    <section>
+        <ul class="file-gallery">
+            <li class="file-gallery__item glass glass--bg" v-for="(file, i) in filesData" v-bind:key="file[i]">
+                {{ file }}
+            </li>
+        </ul>
+    </section>
 </template>
 
 <script>
-export default {
-    props: {
-        filesData : Array
-    },
+    export default {
+        props: {
+            filesData: Array,
+        },
 
-    data(){
-        return {
-
-        }
-    }
-
-}
+        data() {
+            return {};
+        },
+    };
 </script>
 
 <style lang="scss" scoped>
-    .file-gallery{
+    @import "../../src/scss/styles.scss";
+
+    .file-gallery {
         display: flex;
         width: 100%;
         flex-wrap: wrap;
         gap: 1em;
 
-
-        &__item{
+        &__item {
             padding: 1em;
             // background: var(--main);
-            color: var(--bg);
-            border-radius: 5px;
+            // color: var(--bg);
+            border-radius: $bRadius;
             // box-shadow: 6px 6px 12px var(--main-trans);
         }
     }
