4a8062a0f4220f48c619b94597fb789ddf00afdc
miracleonyenma <miracleficient@gmail.com>
2021-01-22 20:19:16 +0100

    update: add custom event for update log and files

--- defaultFE/src/components/fileInput.vue
+++ defaultFE/src/components/fileInput.vue
@@ -1,111 +1,140 @@
 <template>
-    <div class="container">
-        <div class="input-wrapper glass glass--bg">
-            <label for="fileInput">Choose File</label>
-            <input type="file" name="fileInput" id="fileInput" ref="fileInput" v-on:change="handleFileUpload()">
-            <!-- ref is used to register a reference to an element or a child component. The reference will be registered under the parent component’s $refs object -->
-        </div>
-        <button class="btn glass glass--bg" v-on:click="submitFile()">Upload</button>
+  <div class="container">
+    <div class="input-wrapper glass glass--bg">
+      <label for="fileInput">Choose File</label>
+      <input
+        type="file"
+        name="fileInput"
+        id="fileInput"
+        ref="fileInput"
+        v-on:change="handleFileUpload()"
+      />
+      <!-- ref is used to register a reference to an element or a child component. The reference will be registered under the parent component’s $refs object -->
     </div>
+    <button class="btn glass glass--bg" v-on:click="submitFile()">
+      Upload
+    </button>
+  </div>
 </template>
 
 <script>
-    import axios from 'axios';
+import axios from "axios";
 
-    export default {
-        data() {
-            return {
-                // url: process.env.VUE_APP_API_URL,
-                // store file data
-                file: ''
-            }
-        },
+export default {
+  data() {
+    return {
+      // url: process.env.VUE_APP_API_URL,
+      // store file data
+      file: "",
+      data: {},
+    };
+  },
 
-        props: {
-            url:String
-        },
+  props: {
+    url: String,
+    logs: Array,
+  },
 
-        methods: {
-            handleFileUpload() {
-                // get first file from input ref once changed
-                this.file = this.$refs.fileInput.files[0];
-                console.log(this.file);
+  methods: {
+    // updateLog: (data) => {
+    //   this.$emit("update-log", data);
+    // },
+    updateFiles() {
+      axios.get(this.url + "/files").then((res) => {
+          console.log(res.data);
 
-            },
-            submitFile() {
-                let formData = new FormData();
+          let files = res.data.data;
+          this.$emit("update-log", res.data);
+          this.$emit("update-files", files);
+      }).catch(err => {
+          console.error(err);
+      })
 
-                // https://developer.mozilla.org/en-US/docs/Web/API/FormData/append
-                formData.append('file', this.file);
+    },
+    handleFileUpload() {
+      // get first file from input ref once changed
+      this.file = this.$refs.fileInput.files[0];
+      console.log(this.file);
+    },
+    submitFile() {
+      let formData = new FormData();
 
-                // const sendFile = async () =>{
-                //     // try{
-                //     //     const res = await axios.post(this.url + '/single-file',
-                //     //         formData,
-                //     //         {
-                //     //             headers: {
-                //     //                 'Content-Type': 'multipart/form-data'
-                //     //             }
-                //     //         }
-                //     //     );
+      // https://developer.mozilla.org/en-US/docs/Web/API/FormData/append
+      formData.append("file", this.file);
 
-                //     //     console.log(res.data);
+      axios
+        .post(this.url + "/single-file", formData, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        })
+        .then(
+          (res) => {
+            console.log(res.data);
+            this.data = res.data;
+            this.$emit("update-log", res.data);
+            this.updateFiles();
+          },
+          (err) => {
+            console.error(err);
+          }
+        );
 
-                //     // } catch(err){
-                //     //     console.error(err, res.data)
-                //     // }
+      // const sendFile = async () =>{
+      //     // try{
+      //     //     const res = await axios.post(this.url + '/single-file',
+      //     //         formData,
+      //     //         {
+      //     //             headers: {
+      //     //                 'Content-Type': 'multipart/form-data'
+      //     //             }
+      //     //         }
+      //     //     );
 
-                //     axios.post(this.url + '/single-file',
-                //         formData,
-                //         {
-                //             headers: {
-                //                 'Content-Type': 'multipart/form-data'
-                //             }
-                //         }
-                //     ).then(res => {
-                //         console.log(res.data)
-                //     }, err => {
-                //         console.error(err)
-                //     })
-                // };
+      //     //     console.log(res.data);
 
-                // sendFile();
+      //     // } catch(err){
+      //     //     console.error(err, res.data)
+      //     // }
 
-                    axios.post(this.url + '/single-file',
-                        formData,
-                        {
-                            headers: {
-                                'Content-Type': 'multipart/form-data'
-                            }
-                        }
-                    ).then(res => {
-                        console.log(res.data)
-                    }, err => {
-                        console.error(err)
-                    })
+      //     axios.post(this.url + '/single-file',
+      //         formData,
+      //         {
+      //             headers: {
+      //                 'Content-Type': 'multipart/form-data'
+      //             }
+      //         }
+      //     ).then(res => {
+      //         console.log(res.data)
+      //     }, err => {
+      //         console.error(err)
+      //     })
+      // };
 
-            }
-        },
+      // sendFile();
+    },
+  },
 
-        mounted(){
-            console.log(process.env);
-        }
-    }
+  mounted() {
+    console.log(process.env);
+  },
+};
 </script>
 
-<style scoped>
-    .input-wrapper{
-        border-radius: 5px;
-        padding: 2em 1em;
-        margin-bottom: 2em;
-    }
+<style lang="scss" scoped>
+@import "../../src/scss/styles.scss";
+.input-wrapper {
+  border-radius: $bRadius;
+  padding: 2em 1em;
+  margin-bottom: 2em;
+}
 
-    input[type="file"]{
-        outline: none;
-    }
+input[type="file"] {
+  outline: none;
+}
 
-    label[for="fileInput"]{
-        display: block;
-        margin-bottom: 1em;
-    }
+label[for="fileInput"] {
+  display: block;
+  margin-bottom: 1em;
+}
 </style>
