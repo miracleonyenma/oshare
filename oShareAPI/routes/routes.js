99565894b98caa3096379e58bd72b9041ffc5ac1
miracleonyenma <miracleficient@gmail.com>
2021-01-25 12:04:11 +0100

    feat: create routing for static files

--- oShareAPI/routes/routes.js
+++ oShareAPI/routes/routes.js
@@ -1,14 +1,20 @@
 const filesRoutes = require("./files");
 
-const appRouter = (app, fs, path) => {
+const appRouter = (app, express, fs, sharp, path) => {
+    const staticRoute = '/static';
+
+    // serve static files
+    app.use(staticRoute, express.static(path))
 
     app.get("/", (req, res) => {
-        console.log(req)
+        // console.log(req)
+        console.log(path)
         res.send("Hola")
     });
 
-    filesRoutes(app, fs, path);
+    filesRoutes(app, fs, sharp, staticRoute, path);
 
-}
+};
+
 
 module.exports = appRouter;
